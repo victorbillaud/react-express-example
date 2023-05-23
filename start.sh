74c8edeaa1a4c0fb88adc521ea1795e75aece017
victorbillaud <victorbillaud@gmail.com>
2023-05-23 11:01:14 +0200

    build sh

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@ npm start &
 
 echo "Start frontend"
 cd client && npm install && npm run start
+
+# Your existing build steps
+
+# Signal build completion to Jenkins
+# Example using Jenkins CLI: mark the build as successful
+java -jar jenkins-cli.jar -s localhost:8080 mark-build-as-successful $BUILD_NUMBER
