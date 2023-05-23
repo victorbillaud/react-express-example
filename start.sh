6773537fa9b417d79804bbba40e6882212184eb7
victorbillaud <victorbillaud@gmail.com>
2023-05-23 10:58:50 +0200

    build sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ echo "Start backend"
 npm start &
 
 echo "Start frontend"
-cd client && npm install && npm install -g serve && npm run build && serve -s build
+cd client && npm install && npm run start
